66fd449b9c7c571c73fc2d7db9fb2fe05e17b015
Agustin Embuena <agustin.embuena@kometasoft.com>
2014-03-20 10:47:46 +0100

    posesionando el campo y un player

--- GameLayer.cpp
+++ GameLayer.cpp
@@ -40,55 +40,54 @@ bool GameLayer::init()
     
 	//get screen size
 	_screenSize = CCDirector::sharedDirector()->getWinSize();
-	
+
     
 	//1. add court image
     CCSprite * court = CCSprite::create("court.png");
     court->setPosition(ccp(_screenSize.width * 0.5, _screenSize.height * 0.5));
+    court->cocos2d::CCNode::setRotation(90.0);
     this->addChild(court);
     
     
 	//2. add players
 	_player1A =  GameSprite::gameSpriteWithFile("mallet.png");
-	_player1A->setPosition(ccp(_screenSize.width * 0.5, _player1A->radius() ));
-    this->addChild(_player1A, 0, kPlayer1Tag);
+	_player1A->setPosition(ccp(_screenSize.width * 0.75, _player1A->radius() ));
+    this->addChild(_player1A, 1, kPlayer1Tag);
 	_player1B =  GameSprite::gameSpriteWithFile("mallet.png");
-	_player1B->setPosition(ccp(_screenSize.width * 0.5, _player1B->radius() * 2));
-    this->addChild(_player1B, 0, kPlayer1Tag);
+	_player1B->setPosition(ccp(_screenSize.width * 0.5, _player1B->radius() ));
+//    this->addChild(_player1B, 0, kPlayer1Tag);
     _player1C =  GameSprite::gameSpriteWithFile("mallet.png");
-	_player1C->setPosition(ccp(_screenSize.width * 0.5, _player1C->radius() * 4));
-    this->addChild(_player1C, 0, kPlayer1Tag);
+	_player1C->setPosition(ccp(_screenSize.width * 0.25, _player1C->radius()));
+//    this->addChild(_player1C, 0, kPlayer1Tag);
     
 	_player2A =  GameSprite::gameSpriteWithFile("mallet.png");
 	_player2A->setPosition(ccp(_screenSize.width * 0.5, _player2A->radius() ));
-    this->addChild(_player2A, 0, kPlayer1Tag);
+//    this->addChild(_player2A, 0, kPlayer2Tag);
 	_player2B =  GameSprite::gameSpriteWithFile("mallet.png");
-	_player2B->setPosition(ccp(_screenSize.width * 0.5, _player2B->radius() * 2));
-    this->addChild(_player2B, 0, kPlayer1Tag);
+	_player2B->setPosition(ccp(_screenSize.width * 0.5, _player2B->radius()));
+//    this->addChild(_player2B, 0, kPlayer2Tag);
     _player2C =  GameSprite::gameSpriteWithFile("mallet.png");
-	_player2C->setPosition(ccp(_screenSize.width * 0.5, _player2C->radius() * 4));
-    this->addChild(_player2C, 0, kPlayer1Tag);
+	_player2C->setPosition(ccp(_screenSize.width * 0.5, _player2C->radius()));
+//    this->addChild(_player2C, 0, kPlayer2Tag);
 	
 	_players = CCArray::create(_player1A, _player1B, _player1C, _player2A, _player2B, _player2C, NULL);
     _players->retain();
 	
-    
 	//3. add puck
 	_ball = GameSprite::gameSpriteWithFile("puck.png");
-    _ball->setPosition(ccp(_screenSize.width * 0.5, _screenSize.height * 0.5 - 2 * _ball->radius()));
-    this->addChild(_ball);
-    
+    _ball->setPosition(ccp(_screenSize.height * 0.5, _screenSize.width * 0.5 - 2 * _ball->radius()));
+//    this->addChild(_ball);
     
 	//4. add score display
     _player1ScoreLabel = CCLabelTTF::create("0", "Arial", 60);
     _player1ScoreLabel->setPosition(ccp(_screenSize.width - 60, _screenSize.height * 0.5 - 80));
     _player1ScoreLabel->setRotation(90);
-    this->addChild(_player1ScoreLabel);
+//    this->addChild(_player1ScoreLabel);
 	
     _player2ScoreLabel = CCLabelTTF::create("0", "Arial", 60);
     _player2ScoreLabel->setPosition(ccp(_screenSize.width - 60, _screenSize.height * 0.5 + 80));
     _player2ScoreLabel->setRotation(90);
-    this->addChild(_player2ScoreLabel);
+//    this->addChild(_player2ScoreLabel);
     
     
 	//listen for touches
@@ -207,133 +206,133 @@ void GameLayer::update (float dt) {
 	
 	
 	//update puck
-    CCPoint ballNextPosition = _ball->getNextPosition();
-    CCPoint ballVector = _ball->getVector();
-    ballVector = ccpMult(ballVector, 0.98f);
-    
-    ballNextPosition.x += ballVector.x;
-    ballNextPosition.y += ballVector.y;
-    
-	//test for puck and mallet collision
-	float squared_radii = pow(_player1A->radius() + _ball->radius(), 2);
-    
-    GameSprite * player;
-    CCPoint playerNextPosition;
-    CCPoint playerVector;
-	for (int p = 0; p < _players->count(); p++) {
-		
-		player = (GameSprite *) _players->objectAtIndex(p);
-		playerNextPosition = player->getNextPosition();
-        playerVector = player->getVector();
-        
-		float diffx = ballNextPosition.x - player->getPositionX();
-		float diffy = ballNextPosition.y - player->getPositionY();
-		
-		float distance1 = pow(diffx, 2) + pow(diffy, 2);
-		float distance2 = pow(_ball->getPositionX() - playerNextPosition.x, 2) + pow(_ball->getPositionY() - playerNextPosition.y, 2);
-		
-		if (distance1 <= squared_radii || distance2 <= squared_radii) {
-            
-          	float mag_ball = pow(ballVector.x, 2) + pow(ballVector.y, 2);
-			float mag_player = pow (playerVector.x, 2) + pow (playerVector.y, 2);
-          	
-			float force = sqrt(mag_ball + mag_player);
-			float angle = atan2(diffy, diffx);
-			
-			ballVector.x = force * cos(angle);
-			ballVector.y = (force * sin(angle));
-			
-            ballNextPosition.x = playerNextPosition.x + (player->radius() + _ball->radius() + force) * cos(angle);
-            ballNextPosition.y = playerNextPosition.y + (player->radius() + _ball->radius() + force) * sin(angle);
-            
-            SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
-		}
-	}
-	
-	//check collision of ball and sides
-	if (ballNextPosition.x < _ball->radius()) {
-		ballNextPosition.x = _ball->radius();
-        ballVector.x *= -0.8f;
-        SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
-	}
-	
-	if (ballNextPosition.x > _screenSize.width - _ball->radius()) {
-		ballNextPosition.x = _screenSize.width - _ball->radius();
-		ballVector.x *= -0.8f;
-        SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
-	}
-    //ball and top of the court
-	if (ballNextPosition.y > _screenSize.height - _ball->radius()) {
-		if (_ball->getPosition().x < _screenSize.width * 0.5f - GOAL_WIDTH * 0.5f ||
-			_ball->getPosition().x > _screenSize.width * 0.5f + GOAL_WIDTH * 0.5f) {
-			ballNextPosition.y = _screenSize.height - _ball->radius();
-			ballVector.y *= -0.8f;
-            SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
-		}
-	}
-	//ball and bottom of the court
-	if (ballNextPosition.y < _ball->radius() ) {
-		if (_ball->getPosition().x < _screenSize.width * 0.5f - GOAL_WIDTH * 0.5f ||
-			_ball->getPosition().x > _screenSize.width * 0.5f + GOAL_WIDTH * 0.5f) {
-			ballNextPosition.y = _ball->radius();
-			ballVector.y *= -0.8f;
-            SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
-		}
-	}
-    
-    //finally, after all checks, update ball's vector and next position
-    _ball->setVector(ballVector);
-    _ball->setNextPosition(ballNextPosition);
-    
-    
-    //check for goals!
-    if (ballNextPosition.y  < -_ball->radius() * 2) {
-        this->playerScore(2);
-        
-    }
-    if (ballNextPosition.y > _screenSize.height + _ball->radius() * 2) {
-        this->playerScore(1);
-    }
-	
-    //move pieces to next position
-	_player1A->setPosition(_player1A->getNextPosition());
-    _player2A->setPosition(_player2A->getNextPosition());
-	_ball->setPosition(_ball->getNextPosition());
-	
-}
-
-void GameLayer::playerScore (int player) {
-    
-    SimpleAudioEngine::sharedEngine()->playEffect("score.wav");
-    
-    _ball->setVector(CCPointZero);
-    
-    char score_buffer[10];
-    
-    //if player 1 scored...
-    if (player == 1) {
-        
-        _player1Score++;
-        sprintf(score_buffer,"%i", _player1Score);
-        _player1ScoreLabel->setString(score_buffer);
-        //move ball to player 2 court
-        _ball->setNextPosition(ccp(_screenSize.width * 0.5, _screenSize.height * 0.5 + 2 * _ball->radius()));
-        
-        //if player 2 scored...
-    } else {
-        
-        _player2Score++;
-        sprintf(score_buffer,"%i", _player2Score);
-        _player2ScoreLabel->setString(score_buffer);
-        //move ball to player 1 court
-        _ball->setNextPosition(ccp(_screenSize.width * 0.5, _screenSize.height * 0.5 - 2 * _ball->radius()));
-        
-    }
-    //move players to original position
-    _player1A->setPosition(ccp(_screenSize.width * 0.5, _player1A->radius() * 2));
-    _player2A->setPosition(ccp(_screenSize.width * 0.5, _screenSize.height - _player1A->radius() * 2));
-	
-	//clear current touches
-    _player1A->setTouch(NULL);
-    _player2A->setTouch(NULL);
+//    CCPoint ballNextPosition = _ball->getNextPosition();
+//    CCPoint ballVector = _ball->getVector();
+//    ballVector = ccpMult(ballVector, 0.98f);
+//    
+//    ballNextPosition.x += ballVector.x;
+//    ballNextPosition.y += ballVector.y;
+//    
+//	//test for puck and mallet collision
+//	float squared_radii = pow(_player1A->radius() + _ball->radius(), 2);
+//    
+//    GameSprite * player;
+//    CCPoint playerNextPosition;
+//    CCPoint playerVector;
+//	for (int p = 0; p < _players->count(); p++) {
+//		
+//		player = (GameSprite *) _players->objectAtIndex(p);
+//		playerNextPosition = player->getNextPosition();
+//        playerVector = player->getVector();
+//        
+//		float diffx = ballNextPosition.x - player->getPositionX();
+//		float diffy = ballNextPosition.y - player->getPositionY();
+//		
+//		float distance1 = pow(diffx, 2) + pow(diffy, 2);
+//		float distance2 = pow(_ball->getPositionX() - playerNextPosition.x, 2) + pow(_ball->getPositionY() - playerNextPosition.y, 2);
+//		
+//		if (distance1 <= squared_radii || distance2 <= squared_radii) {
+//            
+//          	float mag_ball = pow(ballVector.x, 2) + pow(ballVector.y, 2);
+//			float mag_player = pow (playerVector.x, 2) + pow (playerVector.y, 2);
+//          	
+//			float force = sqrt(mag_ball + mag_player);
+//			float angle = atan2(diffy, diffx);
+//			
+//			ballVector.x = force * cos(angle);
+//			ballVector.y = (force * sin(angle));
+//			
+//            ballNextPosition.x = playerNextPosition.x + (player->radius() + _ball->radius() + force) * cos(angle);
+//            ballNextPosition.y = playerNextPosition.y + (player->radius() + _ball->radius() + force) * sin(angle);
+//            
+//            SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
+//		}
+//	}
+//	
+//	//check collision of ball and sides
+//	if (ballNextPosition.x < _ball->radius()) {
+//		ballNextPosition.x = _ball->radius();
+//        ballVector.x *= -0.8f;
+//        SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
+//	}
+//	
+//	if (ballNextPosition.x > _screenSize.width - _ball->radius()) {
+//		ballNextPosition.x = _screenSize.width - _ball->radius();
+//		ballVector.x *= -0.8f;
+//        SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
+//	}
+//    //ball and top of the court
+//	if (ballNextPosition.y > _screenSize.height - _ball->radius()) {
+//		if (_ball->getPosition().x < _screenSize.width * 0.5f - GOAL_WIDTH * 0.5f ||
+//			_ball->getPosition().x > _screenSize.width * 0.5f + GOAL_WIDTH * 0.5f) {
+//			ballNextPosition.y = _screenSize.height - _ball->radius();
+//			ballVector.y *= -0.8f;
+//            SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
+//		}
+//	}
+//	//ball and bottom of the court
+//	if (ballNextPosition.y < _ball->radius() ) {
+//		if (_ball->getPosition().x < _screenSize.width * 0.5f - GOAL_WIDTH * 0.5f ||
+//			_ball->getPosition().x > _screenSize.width * 0.5f + GOAL_WIDTH * 0.5f) {
+//			ballNextPosition.y = _ball->radius();
+//			ballVector.y *= -0.8f;
+//            SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
+//		}
+//	}
+//    
+//    //finally, after all checks, update ball's vector and next position
+//    _ball->setVector(ballVector);
+//    _ball->setNextPosition(ballNextPosition);
+//    
+//    
+//    //check for goals!
+//    if (ballNextPosition.y  < -_ball->radius() * 2) {
+//        this->playerScore(2);
+//        
+//    }
+//    if (ballNextPosition.y > _screenSize.height + _ball->radius() * 2) {
+//        this->playerScore(1);
+//    }
+//	
+//    //move pieces to next position
+//	_player1A->setPosition(_player1A->getNextPosition());
+//    _player2A->setPosition(_player2A->getNextPosition());
+//	_ball->setPosition(_ball->getNextPosition());
+//	
+//}
+//
+//void GameLayer::playerScore (int player) {
+//    
+//    SimpleAudioEngine::sharedEngine()->playEffect("score.wav");
+//    
+//    _ball->setVector(CCPointZero);
+//    
+//    char score_buffer[10];
+//    
+//    //if player 1 scored...
+//    if (player == 1) {
+//        
+//        _player1Score++;
+//        sprintf(score_buffer,"%i", _player1Score);
+//        _player1ScoreLabel->setString(score_buffer);
+//        //move ball to player 2 court
+//        _ball->setNextPosition(ccp(_screenSize.width * 0.5, _screenSize.height * 0.5 + 2 * _ball->radius()));
+//        
+//        //if player 2 scored...
+//    } else {
+//        
+//        _player2Score++;
+//        sprintf(score_buffer,"%i", _player2Score);
+//        _player2ScoreLabel->setString(score_buffer);
+//        //move ball to player 1 court
+//        _ball->setNextPosition(ccp(_screenSize.width * 0.5, _screenSize.height * 0.5 - 2 * _ball->radius()));
+//        
+//    }
+//    //move players to original position
+//    _player1A->setPosition(ccp(_screenSize.width * 0.5, _player1A->radius() * 2));
+//    _player2A->setPosition(ccp(_screenSize.width * 0.5, _screenSize.height - _player1A->radius() * 2));
+//	
+//	//clear current touches
+//    _player1A->setTouch(NULL);
+//    _player2A->setTouch(NULL);
 }
